b5b3862597d8fb2fe6f4c8d7112c57d22c165e48
Bongardino <bongardino@gmail.com>
2018-08-30 23:18:22 -0400

    THREE HOURS LATER AND ONE GRAPH WORKS it doesnt work correctly but it loads shutup

--- public/index.js
+++ public/index.js
@@ -4,15 +4,15 @@ var HomePage = {
   template: "#home-page",
   data: function() {
     return {
-      message: "Welcome to Vue.js!",
+      message: [],
       calendars: []
     };
   },
   created: function() {
-    axios.get('/api/users/show').then(function(response) {
-      console.log(response.data);
-      this.message = response.data;
-    }.bind(this));
+    // axios.get('/api/users/show').then(function(response) {
+    //   console.log(response.data);
+    //   this.message = response.data;
+    // }.bind(this));
     // axios.get('/api/calendars').then(function(response) {
     //   console.log(response.data);
     //   this.calendars = response.data;
@@ -22,9 +22,37 @@ var HomePage = {
   computed: {}
 };
 
+var LineChart = {
+  template: "#chart-page", // doesn't do anything
+  extends: VueChartJs.Line,
+  data: function() {
+    return {
+      duration: []
+    }
+  },
+  mounted () {
+    axios.get('/api/calendars').then(function(response) {
+      console.log(response.data);
+      this.duration = response.data.map(x => x.duration);
+      console.log("this.duration");
+      console.log(this.duration);
+      this.renderChart({
+        labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+        datasets: [
+          {
+            label: 'Duration',
+            backgroundColor: '#191970',
+            data: this.duration
+          }
+        ]
+      }, {responsive: true, maintainAspectRatio: false})
+    }.bind(this))
+  }
+}
+
 var router = new VueRouter({
   routes: [
-  { path: "/", component: HomePage }
+  { path: "/", component: LineChart }
   ],
   scrollBehavior: function(to, from, savedPosition) {
     return { x: 0, y: 0 };
@@ -35,3 +63,10 @@ var app = new Vue({
   el: "#vue-app",
   router: router
 });
+
+// var vm = new Vue({
+//   el: '.app',
+//   data: {
+//     message: 'Hello World'
+//   }
+// })
